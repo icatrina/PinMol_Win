8bd33660d42f3226de4acd4dabc1363473ae7a09
icatrina <iecatrina@gmail.com>
2017-02-06 17:57:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='PinMol',
-      version='0.1',
+      version='1.0',
       description='Find molecular beacons for live cell imaging of mRNAs',
-      url='https://github.com/icatrina/PinMol',
+      url='https://github.com/icatrina/PinMol_Win',
       author='Irina Catrina',
       author_email='icatrina@hunter.cuny.edu',
       license='Hunter College',
-      #packages=['PinMol'],
+      #packages=['PinMol_Win'],
       install_requires=[
           'pandas<0.19', 'Biopython'],
 
